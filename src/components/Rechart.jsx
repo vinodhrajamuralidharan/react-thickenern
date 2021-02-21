d79ee065bfe260b9ab19059a31bb2356ff8c45ea
VinodhRaja Muralidharan <vinodhraja.muralidharan@gmail.com>
2021-02-21 15:00:26 +0100

    feat: dot overlapping fix

--- src/components/Rechart.jsx
+++ src/components/Rechart.jsx
@@ -17,19 +17,15 @@ export default function Rechart(props) {
   });
   return (
     <LineChart className="line-chart" width={1000} height={600}>
-      <XAxis
-        allowDataOverflow
-        dataKey="category"
-        allowDuplicatedCategory={false}
-      />
-      <YAxis allowDataOverflow dataKey="value" />
+      <XAxis dataKey="category" allowDuplicatedCategory={false} />
+      <YAxis allowDuplicatedCategory={false} dataKey="value" />
       <Tooltip />
       <Legend />
       {data.map(s => (
         <Line
           type="step"
           strokeWidth={2}
-          activeDot={{ stroke: "red", strokeWidth: 1, r: 10 }}
+          activeDot={{ stroke: "red", strokeWidth: 1, r: 3 }}
           dataKey="value"
           data={s.data}
           stroke={"#" + Math.floor(Math.random() * 16777215).toString(16)}
